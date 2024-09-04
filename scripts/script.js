4eea1cc17ca7bebe496e498faa20739efd9175e6
peedroabreu <peedroabreu@gmail.com>
2024-09-03 21:01:10 -0300

    cm1

---
+++ scripts/script.js
@@ -0,0 +1,55 @@
+let openButton = document.querySelector(".profile__EditButton");
+let popup = document.querySelector(".popup");
+let closeButton = document.querySelector(".popup__CloseIcon");
+
+let inputField1 = document.querySelector(".form__field1");
+let inputField2 = document.querySelector(".form__field2");
+
+let profileName = document.querySelector(".profile__info_name");
+let profileProfession = document.querySelector(".profile__info_profession");
+
+function updateFormFields() {
+  let profileNameUpdate = profileName.textContent.trim();
+  let profileProfessionUpdate = profileProfession.textContent.trim();
+
+  inputField1.value = profileNameUpdate;
+  inputField2.value = profileProfessionUpdate;
+}
+
+openButton.addEventListener("click", function () {
+  updateFormFields();
+  popup.classList.add("popup_opened");
+});
+closeButton.addEventListener("click", function () {
+  popup.classList.remove("popup_opened");
+});
+
+let formElement = document.querySelector(".form");
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+
+  let nameInput = document.querySelector(".form__field1");
+  let jobInput = document.querySelector(".form__field2");
+
+  let nameValue = nameInput.value;
+  let jobValue = jobInput.value;
+
+  let nameDisplay = document.querySelector(".profile__info_name");
+  let jobDisplay = document.querySelector(".profile__info_profession");
+
+  nameDisplay.textContent = nameValue;
+  jobDisplay.textContent = jobValue;
+
+  popup.classList.remove("popup_opened");
+}
+
+formElement.addEventListener("submit", handleProfileFormSubmit);
+
+const hearts = document.querySelectorAll(".element__heart");
+
+hearts.forEach((heart) => {
+  heart.addEventListener("click", () => {
+    heart.classList.toggle("clicked");
+  });
+});
